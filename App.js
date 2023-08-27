caf729572b74da802f77dc0e10043405111b407e
Mustafa Anas <ad16mkha@uwcad.it>
2023-08-27 15:07:27 -0400

    setup welcome screen - TODO: Home screen

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import AppNavigation from './src/navigation/navigate.js';
 import { NavigationContainer, StackRouter } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen.js';
@@ -11,19 +10,10 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome">
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Welcome">
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
-        <Stack.Screen name="Welcome" component={WelcomeScreen} options={{ title: 'Welcome' }}/>
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,17 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 export default function WelcomeScreen({ navigation }) {
   return (
-    <SafeAreaView>
-        <View className="container bg-yellow-200 items-center">
-            <View>
-                <Button 
-                    title="Go to Home" 
-                    onPress={() => navigation.navigate("Home")} 
-                />
-            </View>
-        </View>
+    <SafeAreaView className="flex-1 flex justify-around bg-white">
+
+      <View className="space-y-2">
+        <Text style={{fontSize: wp(10)}} className="text-center font-bold text-grey-700">Daii</Text>
+        <Text style={{fontSize: wp(4)}} className="text-center tracking-wider text-grey-600 font-semibold">The AI Assistant</Text>
+      </View>
+
+      <View className="flex-row justify-center">
+        <Image source={require('../../assets/images/bot.png')} style={{width: wp(75), height: wp(75)}}/>
+      </View>
+
+      <TouchableOpacity 
+        onPress={() => navigation.navigate('Home')} 
+        className="bg-emerald-600 mx-5 p-4 rounded-2xl"
+      >
+        <Text style={{fontSize: wp(6)}} className="text-center font-bold text-white">Get Started</Text>
+      </TouchableOpacity>
+
     </SafeAreaView>
   );
 }
